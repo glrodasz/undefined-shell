9e74ff6223c5253dd7f874be657777bdb74bc0f1
Guillermo Rodas <glrodasz@gmail.com>
2024-07-15 20:35:48 +0200

    Change some images and improve SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 export const SITE_TITLE = "Undefined Shell";
-export const SITE_DESCRIPTION = "Programación y desarrollo web";
+export const SITE_AUTHOR = "Guillermo Rodas";
+export const SITE_SHORT_DESCRIPTION = "Programación y desarrollo web";
+export const SITE_DESCRIPTION = `${SITE_TITLE} by ${SITE_AUTHOR} es tu fuente definitiva para todo lo relacionado con la programación y el desarrollo web. En ${SITE_TITLE}, nos apasiona explorar y aprender sobre el desarrollo de productos digitales innovadores, especialmente en el ámbito Full-stack y JavaScript. Ofrecemos tutoriales detallados, análisis de las últimas tendencias tecnológicas y consejos prácticos para desarrolladores de todos los niveles. Únete a nuestra comunidad apasionada por el desarrollo web y lleva tus habilidades al siguiente nivel.`;
 export const SITE_URL = "https://undefined.sh";
