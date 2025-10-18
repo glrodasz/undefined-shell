7b2a72873ea1bb6fcdefb15a7fd06d3dc15513a7
Guillermo Rodas <glrodasz@gmail.com>
2025-10-18 11:45:41 +0200

    chore: commit changes

---
+++ src/i18n/messages/en.ts
@@ -0,0 +1,17 @@
+export const messages = {
+  site: {
+    title: "Undefined Shell",
+    author: "Guillermo Rodas",
+    shortDescription: "Programming and web development",
+    description: "Undefined Shell by Guillermo Rodas is your definitive source for everything related to programming and web development. At Undefined Shell, we are passionate about exploring and learning about innovative digital product development, especially in the Full-stack and JavaScript realm. We offer detailed tutorials, analysis of the latest technological trends, and practical tips for developers of all levels. Join our community passionate about web development and take your skills to the next level.",
+    url: "https://undefined.sh"
+  },
+  blog: {
+    lastUpdatedOn: "last updated on",
+    minRead: "min read"
+  },
+  navigation: {
+    home: "Home",
+    about: "About"
+  }
+} as const;
---
+++ src/i18n/messages/es.ts
@@ -0,0 +1,17 @@
+export const messages = {
+  site: {
+    title: "Undefined Shell",
+    author: "Guillermo Rodas",
+    shortDescription: "Programación y desarrollo web",
+    description: "Undefined Shell by Guillermo Rodas es tu fuente definitiva para todo lo relacionado con la programación y el desarrollo web. En Undefined Shell, nos apasiona explorar y aprender sobre el desarrollo de productos digitales innovadores, especialmente en el ámbito Full-stack y JavaScript. Ofrecemos tutoriales detallados, análisis de las últimas tendencias tecnológicas y consejos prácticos para desarrolladores de todos los niveles. Únete a nuestra comunidad apasionada por el desarrollo web y lleva tus habilidades al siguiente nivel.",
+    url: "https://undefined.sh"
+  },
+  blog: {
+    lastUpdatedOn: "última actualización el",
+    minRead: "min de lectura"
+  },
+  navigation: {
+    home: "Inicio",
+    about: "Acerca"
+  }
+} as const;
